30a1d00c635490ce018105cb52309f4fe30f146d
Justin Guese <guese.justin@gmail.com>
2023-07-07 11:13:39 +0200

    first draft

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # python-openobserve
 A python connector to submit information to OpenObserve (https://github.com/openobserve/openobserve)
+
+The idea is to have a similar python connector to the "Elasticsearch" package, which allows a 1:1 replacement of the "Elasticsearch" package with the "OpenObserve" package.
+
+OpenObserve is way more lightweight than Elasticsearch, and it is open source, like everything should be.
+
+## install
+
+`pip install python-openobserve`
+
+## usage
+
+see [example.ipynb](example.ipynb) for a full example
+
+```python
+from python_openobserve.openobserve import OpenObserve
+
+OO = OpenObserve(user = "root@example.com", password = "Complexpass#123")
+
+from datetime import datetime
+from random import random
+from pprint import pprint
+document = {
+    "@timestamp" : datetime.utcnow(),
+    "component" : "testagent",
+    "action" : "buy", 
+    "amount" : random() * 100,
+    "portfolio" : {
+        "USD" : random() * 100.0,
+        "BTC" : 0.1 + random() * 0.1
+    }
+}
+pprint(document)
+
+# insert document
+OO.index("dd", document)
+
+#search
+# example sql parsing helper
+sql = 'SELECT * FROM "dd"'
+results = OO.search(sql)
+print(f"got {len(results)} results")
+pprint(results)
+```
---
+++ python_openobserve/openobserve.py
@@ -0,0 +1,87 @@
+import requests
+import base64
+from datetime import datetime, timedelta
+from collections.abc import MutableMapping
+from typing import List, Dict
+import sqlglot
+import json
+
+
+def flatten(dictionary, parent_key='', separator='.'):
+    items = []
+    for key, value in dictionary.items():
+        new_key = parent_key + separator + key if parent_key else key
+        if isinstance(value, MutableMapping):
+            items.extend(flatten(value, new_key, separator=separator).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+class OpenObserve:
+    def __init__(self, user, password, organisation = "default", host = "http://localhost:5080") -> None:
+        bas64encoded_creds = base64.b64encode(bytes(user + ":" + password, "utf-8")).decode("utf-8")
+        self.openobserve_url = host + "/api/" + organisation + "/" + "[STREAM]" 
+        self.headers =  {'Content-Type': 'application/x-www-form-urlencoded', "Authorization": "Basic " + bas64encoded_creds}
+
+    def __timestampConvert(self, timestamp: datetime) -> int:
+        return timestamp.timestamp() * 1000
+    
+    def __unixTimestampConvert(self, timestamp: int) -> datetime:
+        try: 
+            timestamp = datetime.fromtimestamp(timestamp/1000000)
+        except:
+            print("could not convert timestamp: " + str(timestamp))
+        return timestamp
+    
+    def __intts2datetime(self, flatdict: dict) -> dict:
+        for key, val in flatdict.items():
+            if "time" in key:
+                flatdict[key] = self.__unixTimestampConvert(val)
+        return flatdict
+
+    def __datetime2Str(self, flatdict: dict) -> dict:
+        for key, val in flatdict.items():
+            if isinstance(val, datetime):
+                flatdict[key] = self.__timestampConvert(val)
+        return flatdict
+
+    def index(self, index: str, document: dict):
+        assert isinstance(document, dict), "document must be a dict"
+        # expects a flattened json
+        document = flatten(document)
+        document = self.__datetime2Str(document)
+
+        res = requests.post(self.openobserve_url.replace("[STREAM]", index) + "/_json", headers=self.headers, json=[document])
+        if res.status_code != 200:
+            raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+        res = res.json()
+        if res["status"][0]["failed"] > 0:
+            raise Exception(f"Openobserve index failed. {res['status'][0]['error']}. document: {document}")
+        return res
+
+    def search(self, sql: str, start_time: datetime = 0, end_time: datetime = 0) -> List[Dict]:
+        if isinstance(start_time, datetime):
+            # convert to unixtime 
+            start_time = self.__timestampConvert(start_time)
+        if isinstance(end_time, datetime):
+            # convert to unixtime 
+            end_time = self.__timestampConvert(end_time)
+
+        # verify sql
+        try:
+            sqlglot.transpile(sql)
+        except sqlglot.errors.ParseError as e:
+            raise e
+
+        query = {"query" : {
+                    "sql": sql,
+                    "start": start_time,
+                    "end": end_time
+                }}
+        res = requests.post(self.openobserve_url.replace("/[STREAM]", "") + "/_search", json = query, headers=self.headers)
+        if res.status_code != 200:
+            raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}. url: {res.url}")
+        # timestamp back convert
+        res = res.json()["hits"]
+        res = [self.__intts2datetime(x) for x in res]
+        return res
