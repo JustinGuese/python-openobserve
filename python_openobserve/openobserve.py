22f6722a504c5d1aafdaedb84663c2afc20f8d18
juju4 <juju4@users.noreply.github.com>
2025-03-02 22:20:45 +0000

    feat: more generic export and import object functions - ok

--- python_openobserve/openobserve.py
+++ python_openobserve/openobserve.py
@@ -201,34 +201,49 @@ class OpenObserve:
         outformat2 = outformat
         if outformat == 'csv' or outformat == 'xlsx':
             outformat2 = 'df'
-        functions1 = self.list_functions(verbosity=verbosity, outformat=outformat2)
-        pipelines1 = self.list_pipelines(verbosity=verbosity, outformat=outformat2)
-        alerts1 = self.list_alerts(verbosity=verbosity, outformat=outformat2)
-        dashboards1 = self.list_dashboards(verbosity=verbosity, outformat=outformat2)
-        users1 = self.list_users(verbosity=verbosity, outformat=outformat2)
+        functions1 = self.list_objects('functions', verbosity=verbosity, outformat=outformat2)
+        pipelines1 = self.list_objects('pipelines', verbosity=verbosity, outformat=outformat2)
+        alerts1 = self.list_objects('alerts', verbosity=verbosity, outformat=outformat2)
+        alerts_destinations1 = self.list_objects('alerts/destinations', verbosity=verbosity, outformat=outformat2)
+        alerts_templates1 = self.list_objects('alerts/templates', verbosity=verbosity, outformat=outformat2)
+        dashboards1 = self.list_objects('dashboards', verbosity=verbosity, outformat=outformat2)
+        streams1 = self.list_objects('streams', verbosity=verbosity, outformat=outformat2)
+        users1 = self.list_objects('users', verbosity=verbosity, outformat=outformat2)
         if outformat == 'csv':
-            functions1.to_csv(f"{file_path}-functions.csv")
-            pipelines1.to_csv(f"{file_path}-pipelines.csv")
-            alerts1.to_csv(f"{file_path}-alerts.csv")
-            dashboards1.to_csv(f"{file_path}-dashboards.csv")
-            users1.to_csv(f"{file_path}-users.csv")
+            functions1.to_csv(f"{file_path}functions.csv")
+            pipelines1.to_csv(f"{file_path}pipelines.csv")
+            alerts1.to_csv(f"{file_path}alerts.csv")
+            alerts_destinations1.to_csv(f"{file_path}alerts-destinations.csv")
+            alerts_templates1.to_csv(f"{file_path}alerts-templates.csv")
+            dashboards1.to_csv(f"{file_path}dashboards.csv")
+            streams1.to_csv(f"{file_path}streams.csv")
+            users1.to_csv(f"{file_path}users.csv")
         elif outformat == 'xlsx':
-            functions1.to_excel(f"{file_path}-functions.xlsx")
-            pipelines1.to_excel(f"{file_path}-pipelines.xlsx")
-            alerts1.to_excel(f"{file_path}-alerts.xlsx")
-            dashboards1.to_excel(f"{file_path}-dashboards.xlsx")
-            users1.to_excel(f"{file_path}-users.xlsx")
+            functions1.to_excel(f"{file_path}functions.xlsx")
+            pipelines1.to_excel(f"{file_path}pipelines.xlsx")
+            alerts1.to_excel(f"{file_path}alerts.xlsx")
+            alerts_destinations1.to_excel(f"{file_path}alerts-destinations.xlsx")
+            alerts_templates1.to_excel(f"{file_path}alerts-templates.xlsx")
+            dashboards1.to_excel(f"{file_path}dashboards.xlsx")
+            streams1.to_excel(f"{file_path}streams.xlsx")
+            users1.to_excel(f"{file_path}users.xlsx")
         else:
             # default json
-            with open(f"{file_path}-functions.json", 'w', encoding='utf-8') as f:
+            with open(f"{file_path}functions.json", 'w', encoding='utf-8') as f:
                 json.dump(functions1, f, ensure_ascii=False, indent=4)
-            with open(f"{file_path}-pipelines.json", 'w', encoding='utf-8') as f:
+            with open(f"{file_path}pipelines.json", 'w', encoding='utf-8') as f:
                 json.dump(pipelines1, f, ensure_ascii=False, indent=4)
-            with open(f"{file_path}-alerts.json", 'w', encoding='utf-8') as f:
+            with open(f"{file_path}alerts.json", 'w', encoding='utf-8') as f:
                 json.dump(alerts1, f, ensure_ascii=False, indent=4)
-            with open(f"{file_path}-dashboards.json", 'w', encoding='utf-8') as f:
+            with open(f"{file_path}alerts-destinations.json", 'w', encoding='utf-8') as f:
+                json.dump(alerts_destinations1, f, ensure_ascii=False, indent=4)
+            with open(f"{file_path}alerts-templates.json", 'w', encoding='utf-8') as f:
+                json.dump(alerts_templates1, f, ensure_ascii=False, indent=4)
+            with open(f"{file_path}dashboards.json", 'w', encoding='utf-8') as f:
                 json.dump(dashboards1, f, ensure_ascii=False, indent=4)
-            with open(f"{file_path}-users.json", 'w', encoding='utf-8') as f:
+            with open(f"{file_path}streams.json", 'w', encoding='utf-8') as f:
+                json.dump(streams1, f, ensure_ascii=False, indent=4)
+            with open(f"{file_path}users.json", 'w', encoding='utf-8') as f:
                 json.dump(users1, f, ensure_ascii=False, indent=4)
 
     def create_function(self, function_json: str, verbosity: int = 0, outformat: str = 'json'):
@@ -246,6 +261,8 @@ class OpenObserve:
             pprint(f"Return {res.status_code}. Text: {res.text}")
         if res.status_code != requests.codes.ok:
             raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+        if verbosity > 0:
+            pprint(f"Create function completed")
         return True
 
     def update_function(self, function_json: str, verbosity: int = 0, outformat: str = 'json'):
@@ -263,13 +280,14 @@ class OpenObserve:
             pprint(f"Return {res.status_code}. Text: {res.text}")
         if res.status_code != requests.codes.ok:
             raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+        if verbosity > 0:
+            pprint(f"Update function completed")
         return True
 
     def import_functions(self, file_path: str, overwrite: bool = False, verbosity: int = 0, outformat: str = 'json'):
         """
         Import functions from json
         Note: API does not import list of functions, need to do one by one.
-        FIXME! Exception: Openobserve returned 400. Text: Content type error
         """
         url = self.openobserve_url.replace("[STREAM]", f"functions")
         if verbosity > 0:
@@ -293,25 +311,123 @@ class OpenObserve:
                       return res
         return True
 
-    def import_pipelines(self, file_path: str, verbosity: int = 0, outformat: str = 'json'):
+    def list_objects(self, object_type: str, verbosity: int = 0, outformat: str = 'json'):
         """
-        Import pipelines from json
-        Note: API does not import list of pipelines, need to do one by one.
-        FIXME! Exception: Openobserve returned 400. Text: Content type error
+        List available objects for given type
         """
-        url = self.openobserve_url.replace("[STREAM]", f"pipelines")
+        key = 'list'
+        if object_type == 'dashboards':
+            key = 'dashboards'
+        if object_type == 'users':
+            key = 'data'
+        if object_type == 'alerts/destinations' or object_type == 'alerts/templates':
+            key = 0
+        url = self.openobserve_url.replace("[STREAM]", f"{object_type}")
+        res = requests.get(url, headers=self.headers, verify=self.verify)
         if verbosity > 0:
             pprint(url)
+        if res.status_code != requests.codes.ok:
+            raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+        res = res.json()
+        if outformat == 'df':
+            return pandas.json_normalize(res[key])
+        return res
+
+    def create_object(self, object_type: str, object_json: str, verbosity: int = 0):
+        """
+        Create object
+        """
+        url = self.openobserve_url.replace("[STREAM]", f"{object_type}")
+        if verbosity > 1:
+            pprint(f"Create object {object_type} url: {url}")
+        if verbosity > 2:
+            pprint(f"Create object json input: {object_json}")
+        res = requests.post(url, json=object_json, headers=self.headers, verify=self.verify)
+        if verbosity > 1:
+            pprint(f"Return {res.status_code}. Text: {res.text}")
+        if res.status_code != requests.codes.ok:
+            raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+            # pprint(f"Openobserve returned {res.status_code}. Text: {res.text}")
+            # return False
+        if verbosity > 0:
+            pprint(f"Create object completed")
+        return True
+
+    def update_object(self, object_type: str, object_json: str, verbosity: int = 0):
+        """
+        Update object
+        """
+        url = self.openobserve_url.replace("[STREAM]", f"{object_type}/{object_json['name']}")
+        if verbosity > 1:
+            pprint(f"Update object {object_type} url: {url}")
+        if verbosity > 2:
+            pprint(f"Update object json input: {object_json}")
+        res = requests.put(url, json=object_json, headers=self.headers, verify=self.verify)
+        if verbosity > 3:
+            pprint(f"Return {res.status_code}. Text: {res.text}")
+        if res.status_code != requests.codes.ok:
+            raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+            # pprint(f"Openobserve returned {res.status_code}. Text: {res.text}")
+            # return False
+        if verbosity > 0:
+            pprint(f"Update object completed")
+        return True
+
+    def import_objects(self, object_type: str, file_path: str, overwrite: bool = False, verbosity: int = 0):
+        """
+        Import objects from json
+        Note: API does not import list of objects, need to do one by one.
+        """
+        key = 'list'
+        key2 = 'name'
+        if object_type == 'dashboards':
+            key = 'dashboards'
+            key2 = 'dashboardId'
+        if object_type == 'users':
+            key = 'data'
+            key2 = 'email'
         with open(file_path, 'r') as json_file:
            json_data = json.loads(json_file.read())
-           if verbosity > 2:
+           if verbosity > 3:
                pprint(json_data)
-           for pipeline in json_data['list']:
+           if object_type == 'alerts/destinations' or object_type == 'alerts/templates':
+               json_list = json_data
+           else:
+               try:
+                  json_list = json_data[key]
+               except:
+                  json_list = [ json_data ]
+           for json_object in json_list:
                if verbosity > 0:
-                  pprint(pipeline['name'])
-               if verbosity > 1:
-                  pprint(pipeline)
-               res = requests.post(url, json=pipeline, headers=self.headers, verify=self.verify)
-               if res.status_code != requests.codes.ok:
-                    raise Exception(f"Openobserve returned {res.status_code}. Text: {res.text}")
+                  pprint(f"Try to create {object_type} {json_object[key2]}...")
+               if verbosity > 2:
+                  pprint(json_object)
+               try:
+                  res = self.create_object(object_type, json_object, verbosity=verbosity)
+                  pprint(f"Create returns {res}.")
+                  return res
+               except Exception as err:
+                  if overwrite:
+                      print(f"Overwrite enabled. Updating object {json_object['name']}")
+                      res = self.update_object(object_type, json_object, verbosity=verbosity)
+                      pprint(f"Update returns {res}.")
         return True
+
+    def config_import(self, object_type: str, file_path: str, overwrite: bool = False, verbosity: int = 0):
+        """
+        Import OpenObserve configuration from json
+        object_type all does everything
+        """
+        if object_type == 'all':
+            self.import_objects('functions', f"{file_path}functions.json", overwrite, verbosity)
+            self.import_objects('pipelines', f"{file_path}pipelines.json", overwrite, verbosity)
+            self.import_objects('alerts', f"{file_path}alerts.json", overwrite, verbosity)
+            self.import_objects('alerts/destinations', f"{file_path}alerts-destinations.json", overwrite, verbosity)
+            self.import_objects('alerts/templates', f"{file_path}alerts-templates.json", overwrite, verbosity)
+            self.import_objects('dashboards', f"{file_path}dashboards.json", overwrite, verbosity)
+            # No CreateStream, only CreateStreamSettings
+            # self.import_objects('streams', f"{file_path}streams.json", overwrite, verbosity)
+            # "Return 400. Text: Json deserialize error: missing field `password` at line 1" = Extra field required
+            # self.import_objects('users', f"{file_path}users.json", overwrite, verbosity)
+        else:
+            self.import_objects(object_type, file_path, overwrite, verbosity)
